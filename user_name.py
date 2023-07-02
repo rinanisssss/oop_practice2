1725878c41ba6768c7101368a045b8de0ceefbe6
rinanisssss <rina.nishizaki0124mafu@gmail.com>
2023-07-02 15:29:48 +0900

    self_intro_method追加

--- user_name.py
+++ user_name.py
@@ -1,4 +1,4 @@
-class UserName:
+class User:
     def __init__(self, name):
         if not (4 <= len(name) <= 20):
             raise ValueError(f"{name}はルール違反です")  # pythonにおいて強制的に起こすのがraise
@@ -8,9 +8,13 @@ class UserName:
     def screen_name(self):
         return self.name.upper()
 
+    def self_introduction(self):
+        print(f"私の名前は{self.name.upper()}です。よろしくお願いします！")
+
 
 # UserNameクラスのインスタンス化
-tanaka = UserName(name="tanaka")
+tanaka = User(name="tanaka")
 
-# 出力
+
 print(tanaka.screen_name())
+print(tanaka.self_introduction())
